edd6bcd1a6dc98276f5852c216f9182c9049627c
Lemuel Coelho Zara <lemuel.czara@gmail.com>
2022-09-29 15:30:29 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static('public'))
+
+app.post('/w', (req, res) => {
+    console.log(req.query, req.body)
+    res.json({
+        result: {
+            body: req.body,
+            query: req.query,
+        }
+    });
+});
+
+app.get('/w', (req, res) => {
+    console.log(req.query, req.body)
+    res.json({
+        result: {
+            body: req.body,
+            query: req.query,
+        }
+    });
+});
+
+app.put('/w', (req, res) => {
+    console.log(req.query, req.body)
+    res.json({
+        result: {
+            body: req.body,
+            query: req.query,
+        }
+    });
+});
+
+app.patch('/w', (req, res) => {
+    console.log(req.query, req.body)
+    res.json({
+        result: {
+            body: req.body,
+            query: req.query,
+        }
+    });
+});
+
+app.listen(9900, () => console.log('Server is running...'));
